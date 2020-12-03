87caae6c98d07127c09195dc718651e68adb20be
Andrej Walilko <awalilko@laptop.marvin.wks.liquidweb.com>
2020-12-03 10:55:07 -0500

    error handling

--- ytdlsplit.sh
+++ ytdlsplit.sh
@@ -26,30 +26,40 @@ printhelp() {
 	echo "ytdlsplit - youtube-dl ffmpeg wrapper for splitting video mp3s
 at description timestamps
 
-usage: ytdlsplit -u \"URL\" -o path/to/output
+usage: ytdlsplit -u \"URL\" -o path/to/output [-q 0]
 
 arguments:
 	-u \"URL\"	quoted URL to youtube-dl compatible video URL
 	-o PATH		path to output directory, will be created if missing
+	-q QUALITY	pass a Vx level (0-9) or specific bitrate (160K)
+			for output audio quality
 
 "
 exit 0
 }
 
 argparse() {
-	while getopts :hu:o: arg; do
+	while getopts :hu:o:q: arg; do
 		case "${arg}" in
 			h) printhelp;;
 			u) url=${OPTARG};;
 			o) output=${OPTARG};;
+			q) quality=${OPTARG};;
 			?) error "Unknown option ${OPTARG}";;
 		esac
 	done
 }
 
 argparse
+#get timestamps
 timestamps=$(mktemp)
 youtube-dl --get-description "$url" 2> /dev/null | awk '{for(i=1; i<=NF; i++){if($i~/[0-9]+\:[0-9]+/){print substr($0,index($0,$i))}}}' > $timestamps
-if ! egrep -q '[0-9]+\:[0-9]+' $timestamps; then
-	error "no timestamps detected in description!"
+if egrep -q '^[^0-9]' $timestamps; then
+	echo "bad timestamps detected (lines must start with numbers separated by colons):" >> ./ytdlsplit.err
+	cat $timestamps >> ./ytdlsplit.err
+	echo "" >> ./ytdlsplit.err
+	error "bad timestamps detected in description! see ./ytdlsplit.err"
 fi
+#download the mp3
+youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output $output'/temp.%(ext)s' "$url" -q
+#calculate song lengths
